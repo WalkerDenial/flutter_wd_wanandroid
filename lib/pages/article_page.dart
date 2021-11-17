e1c0233e9e9853da0a26f1de063d08bbbfb414b9
Walker Denial <WalkerDenial@qq.com>
2021-11-18 00:19:25 +0800

    feat: 开发界面

--- lib/pages/article_page.dart
+++ lib/pages/article_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class ArticlePage extends StatelessWidget {
+class ArticlePage extends StatefulWidget {
   const ArticlePage({Key? key}) : super(key: key);
 
   @override
+  _ArticlePageState createState() => _ArticlePageState();
+}
+
+class _ArticlePageState extends State<ArticlePage> {
+  var _controller = ScrollController();
+  var _isLoading = true;
+  var articles = [];
+  var banners = [];
+  var listTotalSize = 0;
+  var curPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      var maxScroll = _controller.position.maxScrollExtent;
+      var pixels = _controller.position.pixels;
+
+      if (maxScroll == pixels && articles.length < listTotalSize) {
+        _getArticleList();
+      }
+    });
+  }
+
+  void _getArticleList([bool udpate = true]) async {
+    // var data = await Api.
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('content');
+    return Container();
   }
 }
